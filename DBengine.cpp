09fbe207052a04d8012615df767a27e0995243eb
Jennings Thomas Fairchild <jfair23@linux2.cs.tamu.edu>
2015-02-11 14:45:17 -0600

    The first files

---
+++ DBengine.cpp
@@ -0,0 +1,8 @@
+//********************************************************
+// Name			: 
+// Date			: 13 Febuary 2015
+// Subject		: CSCE 315-504
+// Assignment	: Project 2: DBMS
+// Updated		: 11 February 2015
+// Description	: Database Engine where functions will be defined
+//********************************************************
---
+++ DBengine.h
@@ -0,0 +1,48 @@
+//********************************************************
+// Name			: 
+// Date			: 13 Febuary 2015
+// Subject		: CSCE 315-504
+// Assignment	: Project 2: DBMS
+// Updated		: 11 February 2015
+// Description	: Database Engine where functions will be declared
+//********************************************************
+
+void open() {
+
+}
+
+void close() {
+
+}
+
+void exit() {
+
+}
+
+void write() {
+
+}
+
+void show() {
+
+}
+
+void select() {
+
+}
+
+void create() {
+
+}
+
+void insert() {
+
+}
+
+void del() {
+
+}
+
+void update() {
+
+}
