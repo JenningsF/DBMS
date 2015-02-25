bc081b1d1077b46b8b5c481f5a877df7a0201eb8
Quintin Thomas Denman <qtd4066@linux2.cs.tamu.edu>
2015-02-25 17:32:07 -0600

    added comments to parser.h for additional work to be done

--- Parser.h
+++ Parser.h
@@ -1,15 +1,18 @@
 #include <string>
 #include <iostream>
 #include <string>
+//Replace with new header file
 #include "DBengine.h"
 
 using namespace std;
 
+//move to new header file
 enum string_command{eOpen, eClose, eInsert, eCreate, eWrite, eExit, eUpdate, eDelete, eShow};
 string_command hashit(string const& inString);
 enum string_type{eChar, eInt};
 string_type hashtype(string const& inString);
 
+//move to new header file
 struct element {
 	string query_type;
 	string column;
@@ -19,17 +22,24 @@ struct element {
 
 class Parser {
 private:
+	/*Holds input when constructor is called*/
 	string line;
+	/*Parameters used for queries*/
 	string viewName;
 	string fromName;
 	char op;
 	vector<element> query;
+	/*Parameters used for queries and commands*/
+	
+	/*Parameters used for commands*/
+	
 public:
 	Parser();
-	Parser(string l) : line(l) {
-		parse();
-	}
-	void parse();
+	~Parser();
+	//Add bool return
+	void parse(string line);
+	//Rewrite this function to utilize the line variable
+	// getting rid of string l
 	void parse_command(string l);
 	void parse_create(string l);
 	void parse_insert(string l);
@@ -38,6 +48,8 @@ public:
 	bool parse_select(string select_string);
 	element parse_list(string list_string);
 	void parse_query();
+	
+	//Create get functions for private members
 }; 
 
 
