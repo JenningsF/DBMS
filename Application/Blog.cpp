d62ea94f63c7349e78d9ebf49a21b3690984044b
codizzler <crt@riseup.net>
2015-03-06 22:51:06 -0600

    Added useful comments and moved definitions to cpp file.

--- Application/Blog.cpp
+++ Application/Blog.cpp
@@ -48,6 +48,228 @@ int Post::getPostID() {return postId;}
 int Post::getParentID() {return parentId;}
 
 
+/*
+*	Blog Application
+*		Function
+*			Definitions
+*/
+// Constructor
+Blog::Blog() {
+	mainMenu = "[Main Menu]\n\n1. Make a new post\n2. Search for a post\n3. Exit\n\n";
+    searchMenu = "[Search Menu]\n\nSearch by:\n1. Author\n2. Title\n3. Tag(s)\n4. Date\n5. Return to Main Menu\n\n";
+    postMenu = "1. View\n2. Edit\n3. Delete\n4. Comment\n5. Return to Main Menu\n\n";
+	commandPrompt = "* Enter command: ";
+	done = false;	
+}
+
+// This function displays the post menu, allowing for a selection of 
+// view, edit, delete or comment. This function needs it's implementation finished
+void Blog::displayPost(int p) {
+	char option = '0';
+	if (p < 0 || p >= currentPosts.size()) {
+		cout << "\n\n--INCORRECT SELECTION--\n\n";
+		return;
+	}
+	else {
+		cout << '[' << currentPosts[p].getTitle() << "]\n\n" << postMenu << commandPrompt;
+		cin >> option;
+		cout << endl;
+		switch (option) {
+			case '1':
+				cout << "\n\nView option selected\n\n";
+				break;
+			case '2':
+				cout << "\n\nEdit option selected\n\n";
+				break;
+			case '3':
+				cout << "\n\nDelete option selected\n\n";
+				break;
+			case '4':
+				cout << "\n\nComment option selected\n\n";
+				break;
+			case '5':
+				break;
+			default:					
+				cin.clear(); 
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				cout << "Unknown command..\n" << endl;
+				break;
+		}
+	}
+}
+
+// Display intermediate results from search
+// i.e. multiple posts with same author, date, etc..
+void Blog::displayCurrentPosts() {	
+	int index = -1;		
+	for (int i = 1; i <= currentPosts.size(); ++i) {
+		cout << i << ". " << currentPosts[i-1].getTitle() << " (" << currentPosts[i-1].getDatePosted() << ")\n";
+	}
+	cout << currentPosts.size() + 1 << ". Return to Main Menu\n\n";
+	cout << "* Enter ID: ";
+	if (cin >> index);  
+	else {
+		cin.clear(); 
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "\n\nYou break it, you buy it...\n\n";
+		return;	 
+	}
+	// Calls display on selected post
+	displayPost(index);
+}
+
+// Searches for posts that has a matching author
+void Blog::searchAuthor(string name) {
+	for (int i = 0; i < posts.size(); ++i) {
+		if (posts[i].getAuthor() == name) {
+			Post temp(posts[i]);
+			currentPosts.push_back(temp);
+		}
+	}
+	if (currentPosts.size() > 0) {
+		cout << '[' << name << "'s Posts]\n\n";
+		displayCurrentPosts();
+	}
+	else  {
+		cin.clear(); 
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "No Posts Found\n\n";
+	}
+}
+
+// Searches for posts that match the specified title
+void Blog::searchTitle(string title) {
+	char option = '0';
+	for (int i = 0; i < posts.size(); ++i) {
+		if (posts[i].getTitle() == title){
+			Post temp(posts[i]);
+			currentPosts.push_back(temp);
+		}
+	}
+	if (currentPosts.size() > 0) {
+		cout << '[' << title << "'s Posts]\n\n";
+		displayCurrentPosts();
+	}
+	else {
+		cin.clear(); 
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "No Posts Found\n\n";
+	}
+}
+
+// Incomplete search feature, must parse tag argument and
+// insert each tag into vector of tags to compare with Post tags
+void Blog::searchTags(string tag) {
+	vector<string> tags;
+	tags.push_back(tag);
+	// Must parse argument for multiple tags
+	for (int i = 0; i < posts.size(); ++i) {
+		if (posts[i].getTags() == tags)	{
+			Post temp(posts[i]);
+			currentPosts.push_back(temp);
+		}
+	}
+	if (currentPosts.size() > 0) {
+		cout << '[' << tag << "'s Posts]\n\n";
+		//displayCurrentPosts();
+	}
+	else  {
+		cin.clear(); 
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "No Posts Found\n\n";
+	}
+}
+
+// Searches for post that match the specified date
+// parsing date could be added
+void Blog::searchDate(string day) {
+	for (int i = 0; i < posts.size(); ++i) {
+		if (posts[i].getDatePosted() == day){
+			Post temp(posts[i]);
+			currentPosts.push_back(temp);
+		}
+	}
+	if (currentPosts.size() > 0) {
+		cout << '[' << day << "'s Posts]\n\n";
+		displayCurrentPosts();
+	}
+	else  {
+		cin.clear(); 
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "No Posts Found\n\n";
+	}
+}
+
+// Sequence containing search menu
+void Blog::SearchSequence() {
+	char option = '0';
+	string searchFor = "";
+	cout << searchMenu << commandPrompt;
+	cin >> option;
+	cout << endl;
+	switch (option) {
+		case '1':
+			cout << "* Enter author: ";
+			cin >> searchFor;
+			cout << endl;
+			searchAuthor(searchFor);
+			break;
+		case '2':
+			cout << "* Enter title: ";
+			cin >> searchFor;
+			cout << endl;
+			searchTitle(searchFor);
+			break;
+		case '3':
+			cout << "* Enter tags (seperated by commas): ";
+			cin >> searchFor;
+			cout << endl;
+			searchTags(searchFor);
+			break;
+		case '4':
+			cout << "* Enter date: ";
+			cin >> searchFor;
+			cout << endl;
+			searchDate(searchFor);
+			break;
+		case '5':
+			break;
+		default:
+			cin.clear(); 
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Unknown command..\n" << endl;
+			break;
+	}
+}
+
+// Begins main menu of blog application, does not end until Exit option is selected
+void Blog::MenuSequence() {
+	while (!done) {
+		char option = '0';
+		cout << mainMenu << commandPrompt;
+		cin >> option;
+		cout << endl;
+		switch (option) {
+			case '1':
+				cout << "New post selected\n" << endl;
+				break;
+			case '2':
+				SearchSequence();
+				break;
+			case '3':
+				cout << "Exit selected\n" << endl;
+				done = true;
+				break;
+			default:						
+				cin.clear(); 
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				cout << "Unknown command..\n" << endl;
+				break;
+		}
+	}	
+}
+
+
 
 
 
