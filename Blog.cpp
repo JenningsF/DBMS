c981e774f1962da62462ef1a881d331d91adcc9a
Cody Taylor <cody.taylor49@tamu.edu>
2015-03-06 20:36:06 -0600

    Fixed API issue

--- Blog.cpp
+++ Blog.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main() {
 	DBMSParse("CREATE TABLE animals (name VARCHAR(20), kind VARCHAR(8), years INTEGER) PRIMARY KEY (name, kind);");
-	Table tab = getRelation("animals");
+	Table tab = GetRelation("animals");
 	vector<string> first = {"Joe", "dog", "41"};
 	tab.addTuple(first);
 	cout << tab.getTuple(0)[0] << tab.getTuple(0)[1] << endl;
